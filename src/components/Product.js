4448d7e11522e9ef8585a8f5ad947d54a4d3a2e5
md arif <arifahd92@gmail.com>
2023-10-28 13:14:35 +0530

    saved functionality added

--- src/components/Product.js
+++ src/components/Product.js
@@ -6,16 +6,19 @@ import { useNavigate } from "react-router-dom";
 
 export default function Product() {
   const [search, setsearch] = useState("");
+  const [request, setRequest] = useState(true);
   const [products, setproducts] = useState([]);
   const navigate = useNavigate(null);
   const getProducts = async () => {
     try {
       console.log("i got called");
+      setRequest(true);
       const response = await axios.get(`https://dummyjson.com/products`);
       const data = response.data.products;
       console.log(data);
       if (search.length === 0) {
         setproducts(data);
+        setRequest(false);
         return;
       }
       if (search.length > 0) {
@@ -23,12 +26,15 @@ export default function Product() {
           item.title.toLowerCase().includes(search.toLowerCase())
         );
         setproducts(filtered);
+        setRequest(false);
         return;
       }
 
       //console.log(Product?.Product?.label);
     } catch (error) {
       console.log(error.message);
+      alert(error.message);
+      setRequest(false);
     }
   };
   useEffect(() => {
@@ -47,16 +53,48 @@ export default function Product() {
 
     navigate(`/description/${id}`);
   };
+  //handle saved
+  const handleSaved = async () => {
+    try {
+      let savedArr = JSON.parse(localStorage.getItem("saved"));
+      if (savedArr.length === 0) {
+        alert("Cart is empty, view and save some items");
+        return;
+      }
+      console.log("savedArr", savedArr);
+      setRequest(true);
+      setproducts([]);
+      const response = await axios.get(`https://dummyjson.com/products`);
+      const data = response.data.products;
+      const savedItem = data.filter((item) =>
+        savedArr.includes(item.id.toString())
+      );
+      console.log("savedItem", savedItem);
+      setproducts(savedItem);
+      setRequest(false);
+    } catch (error) {
+      alert(error.message);
+    }
+  };
   return (
     <>
       <Search setsearch={setsearch} />
       <hr />
+
+      <div className="savedItem row position-fixed z-3 ">
+        <button className="btn btn-warning" onClick={handleSaved}>
+          saved items
+        </button>
+      </div>
+
       {products.length === 0 && (
         <div className="container position-relative ">
           <div
             className="alert alert-primary absolute text-center"
             role="alert">
-            Your search did not match to any product.
+            {request
+              ? "please wait..."
+              : "Your search did not match to any product."}
           </div>
         </div>
       )}
@@ -78,7 +116,7 @@ export default function Product() {
                     alt="Image 1"
                   />
                 </div>
-                <div className="card-body" style={{ height: "100px" }}>
+                <div className="card-body">
                   <div className="row">
                     <div className="col text-center">
                       <h5 className="card-title text-center">{item.title}</h5>
